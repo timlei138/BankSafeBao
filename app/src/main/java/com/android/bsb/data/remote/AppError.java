4599a51cccdf22966d020ff1952bd548820f6a29
smithlei <itstoneslc@gmail.com>
2018-05-28 20:23:50 +0800

    add code for apiexception

---
+++ app/src/main/java/com/android/bsb/data/remote/AppError.java
@@ -0,0 +1,26 @@
+package com.android.bsb.data.remote;
+
+public class AppError {
+
+
+    /**
+     * 未知错误
+     */
+    public static final int UNKNOWN = 1000;
+    /**
+     * 解析错误
+     */
+    public static final int PARSE_ERROR = 1001;
+    /**
+     * 网络错误
+     */
+    public static final int NETWORD_ERROR = 1002;
+    /**
+     * 协议出错
+     */
+    public static final int HTTP_ERROR = 1003;
+
+
+    public static final int DECODE_ERROR = 1004;
+
+}
